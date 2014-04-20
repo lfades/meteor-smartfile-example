622a74932300ef698ab487a054c606ac7fed58a3
luiskotsuki <luisito453@gmail.com>
2014-04-19 21:54:12 -0500

    first commit

---
+++ client/client.js
@@ -0,0 +1,74 @@
+sf = new SmartFile({
+	publicRootUrl: "https://file.ac/U1hRCXwFVQA/"
+});
+
+// I recommend you open the console to see logs
+
+Meteor.subscribe('smartfile');
+
+Template.smartfile.events({
+	'change input[type=file]': function (e) {
+		var file = e.target.files[0];
+		if(file) {
+			sf.preview(file, function(data) {
+				if(data) {
+					$('#preview-images').append('<img src="' + data.src + '"> ' + data.width +'x' + data.height + ' ' + data.size + ' ' + data.type + ' ' + data.name + '<br>');
+					// controller only save a file, upload more than one file erases the previous
+					sf.upload(file, 'photo', function (error, res) {
+						if(error) {
+							console.log("error uploading the file", error);
+							$('#preview-images').html('');
+							return;
+						}
+						console.log("File uploaded, the path is:" + sf.resolvePublic(res.nameId));
+					});
+				}
+			});
+		}
+		/* the above is the same as this 
+
+			// sf.preview only works with images
+			sf.preview(file, function(data) {
+				if(data)
+					$('#preview-images').append('<img src="' + data.src + '"> ' + data.width +'x' + data.height + ' ' + data.size + ' ' + data.type + ' ' + data.name + '<br>');
+			});
+
+			sf.upload(file, function (error, res) {
+				if(error) {
+					console.log("error uploading the file", error);
+					return;
+				}
+				console.log("File uploaded, the path is:" + sf.resolvePublic(res.nameId));
+			});
+		*/
+	},
+	'click #save': function (e) {
+		if(Meteor.userId()) {
+			var file = sf.getFiles('photo');
+			// when you save the file, the previous file is deleted from user
+			Meteor.users.update(Meteor.userId(), {
+				'$set': {
+					'profile.photo': file
+				}
+			}, function (error) {
+				console.log(error || 'file saved in the user profile');
+			});
+
+			/*
+			Meteor.call('editUserPhoto', function (error) {
+				console.log(error || 'file saved in the user profile');
+			});
+			*/
+		}
+	}
+});
+
+Template.smartfile.helpers({
+	photo: function () {
+		if(Meteor.user()) {
+			var photo = Meteor.user().profile.photo;
+			// you can use in template {{ sfPath photo.nameId }} - is the same
+			return sf.resolvePublic(photo && photo.nameId);
+		}
+	}
+});
---
+++ server/server.js
@@ -0,0 +1,99 @@
+Meteor.users.deny({
+	insert: function () { return true; },
+	update: function (userId, doc, fieldNames, otro) {
+		var photo = sf.getFiles('photo');
+		if(!photo)
+			return true;
+
+		var previousPhoto = doc.profile.photo;
+		if(previousPhoto) {
+			Meteor.defer(function () {
+				sf.rm(previousPhoto.nameId);
+			});
+		}
+
+		// we remove the local log file because it is now saved in the user
+		sf.cleanSfCollection(userId, 'photo');
+		return false;
+	},
+	remove: function () { return true; }
+});
+
+
+// if you like use methods as me
+Meteor.methods({
+	editUserPhoto: function () {
+		var userId = this.userId;
+		if(!userId)
+			throw new Meteor.Error(401, 'no user');
+
+		var photo = sf.getFiles('photo');
+		if(!photo)
+			throw new Meteor.Error(401, 'first upload the image');
+
+		var previousPhoto = Meteor.user().profile.photo;
+		if(previousPhoto) {
+			Meteor.defer(function () {
+				sf.rm(previousPhoto.nameId);
+			});
+		}
+
+		// we remove the local log file because it is now saved in the user
+		sf.cleanSfCollection(userId, 'photo');
+		Meteor.users.update(userId, {
+			'$set': {
+				'profile.photo': photo
+			}
+		});
+	}
+});
+
+sf = new SmartFile();
+
+sf.configure({
+	key: "qvRxfIcUlCTHIF7Y5VjMJWDL7hzMSh",
+	password: "B5aCvdlEgstW7vnOnF85JKC3nDqkS1",
+	basePath: "prueba"/*,
+	fileNameId: function (fileName) {
+		// you can change the nameId here
+		// the filename has no extension
+		return fileName;
+	}*/
+});
+
+sf.allow = function (options) {
+	// you can use this.userId
+	return true;
+};
+
+sf.onUpload = function (result, options) {
+	//result is the smartfile api JSON response
+	console.log("File uploaded to " + result[0].path);
+};
+
+sf.onUploadFail = function (error, options) {
+	console.log("SmartFile returned error", error.statusCode, error.detail);
+};
+
+sf.fileControllers({
+	photo: {
+		ext: ['jpg', 'png'],
+		path: '', // optional, path of storage of the upload relative to basePath
+		size: 300000, // 300 Kb - default is 2 Mb
+		allow: function (options) {
+			// you can use this.userId
+			return true;
+		}
+	}
+	/* 
+	photo: {
+		ext: ['jpg', 'png'] // IS OK
+	}
+	*/
+});
+
+Meteor.publish('smartfile', function() {
+	if(this.userId)
+		return sf.collection.find({'user': this.userId});
+	return [];
+});
